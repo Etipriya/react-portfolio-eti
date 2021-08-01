23ced7b35052b0f9e96d1bb030a419477472afbe
Eti Priya <etipriya@gmail.com>
2021-08-01 15:26:49 +0100

    added app.js and app.css file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navigation from "./components/Navigation";
+import Projects from "./components/Projects";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation />
+      <About />
+      <Projects />
+      <Contact />
     </div>
   );
 }
