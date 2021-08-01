93000e221c030d970c553e42ca5eaa9fc070dcad
Eti Priya <etipriya@gmail.com>
2021-08-01 15:41:49 +0100

    added footer componet

---
+++ src/components/Footer/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer>
+      <p>Copyright &copy; 2021 All Rights Reserved by Eti Priya</p>
+    </footer>
+  );
+}
