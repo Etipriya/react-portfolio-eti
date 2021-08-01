2afd0f84835d0bf2de0d64b8a4b9f40128a3ebbd
Eti Priya <etipriya@gmail.com>
2021-08-01 18:08:41 +0100

    changed image path

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import "./App.css";
 
 // import About from "./components/About";
 // import Contact from "./components/Contact";
-import Footer from "./components/Footer";
+import Navigation from "./components/Navigation";
 import Header from "./components/Header";
 import Home from "./components/Home";
-import Navigation from "./components/Navigation";
+import Footer from "./components/Footer";
 // import Projects from "./components/Projects";
 
 function App() {
--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Card, Container, Row, Col, Image } from "react-bootstrap";
-import img from "../../assets/etipriya-cv.pdf";
+import img from "../../assets/images/eti_pic.jpg";
 
 function About() {
   return (
