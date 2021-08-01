c78ebe36d38a95586597c92d215f998f23d586e6
Eti Priya <etipriya@gmail.com>
2021-08-01 19:49:07 +0100

    changed app.js file

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import "./App.css";
 
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Navigation from "./components/Navigation";
-import Header from "./components/Header";
-// import Home from "./components/Home";
+// import Header from "./components/Header";
+import Home from "./components/Home";
 import Footer from "./components/Footer";
 import Projects from "./components/Projects";
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Navigation />
-        <About />
-        <Header />
-        {/* <Home /> */}
-        <Projects />
-        <Contact />
-        <Footer />
-      </div>
+      <Switch>
+        <div className="App">
+          <Navigation />
+          <Route path="/">
+            <Home />
+          </Route>
+          <Route path="/About">
+            <About />
+          </Route>
+          <Route path="/Projects">
+            <Projects />
+          </Route>
+          <Route path="/Contact">
+            <Contact />
+          </Route>
+          {/* <Navigation />
+          <About />
+          <Header /> */}
+          {/* <Home /> */}
+
+          <Footer />
+        </div>
+      </Switch>
     </Router>
   );
 }
