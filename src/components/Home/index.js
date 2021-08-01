402de421a15a118414b72a4aaf9dd90702553fec
Eti Priya <etipriya@gmail.com>
2021-08-01 15:44:26 +0100

    added home component

---
+++ src/components/Home/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import About from "../About";
+import ContactForm from "../Contact";
+import Projects from "../Projects";
+
+export default function Home() {
+  return (
+    <div>
+      <About />
+      <Projects />
+      <ContactForm />
+    </div>
+  );
+}
