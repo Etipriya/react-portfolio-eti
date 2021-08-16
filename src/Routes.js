c48a2a21a4e20a32baaf18bd0893af313d0378d5
Eti Priya <etipriya@gmail.com>
2021-08-16 17:16:45 +0100

    added routes file

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,23 @@
 import { Switch, Route } from "react-router-dom";
-import { Contact } from "./components/Contact";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
 
 const Routes = () => {
   return (
     <Switch>
-      <Route exact path="/contact">
+      <Route exact path="/About">
+        <About />
+      </Route>
+      <Route exact path="/Projects">
+        <Projects />
+      </Route>
+      <Route exact path="/Contact">
         <Contact />
+      </Route>
+      <Route exact path="/">
+        <Home />
       </Route>
     </Switch>
   );
