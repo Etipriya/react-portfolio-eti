74bfd6c1610e164ffa0d8c254dbadf50bb4df8f8
Eti Priya <etipriya@gmail.com>
2021-08-18 11:08:15 +0100

    changed app.js and home.js

--- src/App.js
+++ src/App.js
@@ -1,41 +1,24 @@
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 import { HashRouter as Router } from "react-router-dom";
+import Navigation from "./components/Navigation";
+import { Switch } from "react-router";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
 
 import "./App.css";
 
 // import About from "./components/About";
 // import Contact from "./components/Contact";
-import Navigation from "./components/Navigation";
 // import Header from "./components/Header";
-import Home from "./components/Home";
-import Footer from "./components/Footer";
 // import Projects from "./components/Projects";
 
 const App = () => {
   return (
-    <Router>
+    <Router basename="/">
       <Navigation />
       <Switch>
         <div className="App">
-          <Navigation />
           <Home />
-
-          {/* <Route path="/">
-            <Home />
-          </Route>
-          <Route path="/About">
-            <About />
-          </Route>
-          <Route path="/Projects">
-            <Projects />
-          </Route>
-          <Route path="/Contact">
-            <Contact />
-          </Route> */}
-          {/* <Navigation />
-          <About />
-          <Header /> */}
-          {/* <Home /> */}
           <Footer />
         </div>
       </Switch>
